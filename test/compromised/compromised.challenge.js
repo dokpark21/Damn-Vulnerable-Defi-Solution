a9025cb25248e18dbfafd4edeae5cdd676dbdff3
BEB 09 박상현 <psh03225@naver.com>
2024-04-18 20:56:09 +0900

    UPDATE: compromised go code with decode private key

--- test/compromised/compromised.challenge.js
+++ test/compromised/compromised.challenge.js
@@ -119,3 +119,61 @@ describe('Compromised challenge', function () {
     expect(await oracle.getMedianPrice('DVNFT')).to.eq(INITIAL_NFT_PRICE);
   });
 });
+
+//go code with decode response to address, private key, public key
+/**
+ package main
+
+import (
+	"encoding/base64"
+	"encoding/hex"
+	"fmt"
+	"strings"
+
+	"github.com/ethereum/go-ethereum/crypto"
+)
+
+func main() {
+	// decoded this string hex value
+	responses := []string{
+		"4d 48 68 6a 4e 6a 63 34 5a 57 59 78 59 57 45 30 4e 54 5a 6b 59 54 59 31 59 7a 5a 6d 59 7a 55 34 4e 6a 46 6b 4e 44 51 34 4f 54 4a 6a 5a 47 5a 68 59 7a 42 6a 4e 6d 4d 34 59 7a 49 31 4e 6a 42 69 5a 6a 42 6a 4f 57 5a 69 59 32 52 68 5a 54 4a 6d 4e 44 63 7a 4e 57 45 35",
+		"4d 48 67 79 4d 44 67 79 4e 44 4a 6a 4e 44 42 68 59 32 52 6d 59 54 6c 6c 5a 44 67 34 4f 57 55 32 4f 44 56 6a 4d 6a 4d 31 4e 44 64 68 59 32 4a 6c 5a 44 6c 69 5a 57 5a 6a 4e 6a 41 7a 4e 7a 46 6c 4f 54 67 33 4e 57 5a 69 59 32 51 33 4d 7a 59 7a 4e 44 42 69 59 6a 51 34",
+	}
+
+	for i, response := range responses {
+		// decode the hex value
+		decoded, err := hex.DecodeString(strings.Replace(response, " ", "", -1))
+		if err != nil {
+			fmt.Printf("Error decoding %d: %s\n", i+1, err)
+			continue
+		}
+
+		result := make([]byte, base64.StdEncoding.DecodedLen(len(decoded)))
+
+		fmt.Printf("Decoded %d. \n ", i+1)
+		fmt.Println(string(decoded))
+		// decode encoding by base64 string
+		fmt.Printf("Base64 encoded %d. \n ", i+1)
+		_, _ = base64.StdEncoding.Decode(result, decoded)
+		fmt.Println(string(result))
+
+		// decode the base64 string to ethereum address
+		pvk, err := crypto.HexToECDSA(string(result[2:]))
+		if err != nil {
+			fmt.Printf("Error decoding %d: %s\n", i+1, err)
+			continue
+		}
+		address := crypto.PubkeyToAddress(pvk.PublicKey).Hex()
+
+		fmt.Printf("Ethereum address %d: %s\n", i+1, address)
+
+	}
+
+}
+
+// Logic
+// 1. Decode the hex value with out space
+// 2. Decode the base64 string to public key
+// 3. Decode the base64 string to ethereum address
+
+ */
